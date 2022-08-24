0820bc5b48b656de28d3730ca0ff12dad7b5d73e
Alena Lisava <lisi4ka.87@mail.ru>
2022-08-24 19:09:10 +0300

    fix:fix

--- sources/myapp.js
+++ sources/myapp.js
@@ -1,16 +1,15 @@
-import "./styles/app.css";
 import {JetApp, EmptyRouter, HashRouter } from "webix-jet";
 
 export default class MyApp extends JetApp{
 	constructor(config){
 		const defaults = {
-			id 		: APPNAME,
+			id 	: APPNAME,
 			version : VERSION,
-			router 	: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
-			debug 	: !PRODUCTION,
-			start 	: "/top/start"
+			router : BUILD_AS_MODULE ? EmptyRouter : HashRouter,
+			debug : !PRODUCTION,
+			start : "/top/start",
+			name: "Spreadsheet"
 		};
-
 		super({ ...defaults, ...config });
 	}
 }
